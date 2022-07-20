1b094eb3fe350aedf40fc86035390400bcfa8c8f
geonryulPark <geonryul0131@gmail.com>
2022-07-20 23:14:16 +0900

    7/20

---
+++ prework_assignment/kotlin/Hello_Kotlin/src/main/kotlin/Hello.kt
@@ -0,0 +1,4 @@
+    fun main(args: Array<String>) {
+        val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+        println(decorations.filter { it.contains('p') })
+    }
