81c897a85b68354d3907c1df7df254652106884d
geonryulPark <geonryul0131@gmail.com>
2022-07-24 17:21:45 +0900

    7/24

---
+++ prework_assignment/kotlin/Hello_Kotlin/src/main/kotlin/example/myapp/main.kt
@@ -0,0 +1,10 @@
+    package example.myapp
+
+    fun buildAquarium() {
+        val myAquarium6 = Aquarium(length = 25, width = 25, height = 40)
+        myAquarium6.printSize()
+    }
+
+    fun main() {
+        buildAquarium()
+    }
